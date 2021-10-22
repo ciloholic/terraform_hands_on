8ea2bc5b185caa1f4b937c48bafbed971adb284d
ciloholic <ciloholic0119@gmail.com>
2021-10-23 05:28:14 +0900

    add Terraform update command to Makefile

--- src/02_versions.tf
+++ src/02_versions.tf
@@ -1,11 +1,11 @@
 terraform {
-  # 本ハンズオンは、Terraform のバージョンを `1.0.9` 以上としています
-  required_version = "~> 1.0.9"
+  # Terraform のバージョンは、ハンズオン作成時の最新としています
+  required_version = "1.0.9"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       # AWS プロバイダのバージョンは、ハンズオン作成時の最新としています
-      version = "~> 3.63"
+      version = "3.63.0"
     }
   }
 }
