7dab614a554c266ab02a3a825ac0c6f1f61c33ae
ciloholic <ciloholic0119@gmail.com>
2021-10-30 23:00:46 +0900

    rename files

--- src/21_ssm.tf
+++ src/21_ssm.tf
@@ -1,8 +1,8 @@
 resource "aws_ssm_parameter" "aurora_master_password" {
-  name  = "/${local.service_config.name}/${local.service_config.env}/aurora-master-password"
-  type  = "SecureString"
+  name   = "/${local.service_config.name}/${local.service_config.env}/aurora-master-password"
+  type   = "SecureString"
   key_id = aws_kms_key.example.key_id
-  value = aws_rds_cluster.example.master_password
+  value  = aws_rds_cluster.example.master_password
 }
 
 resource "aws_ssm_parameter" "database_host" {
